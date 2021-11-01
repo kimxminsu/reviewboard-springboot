cb11cd85c517462449b6412e34ec24b699082ee4
minsu <kimxminsu@gmail.com>
2021-11-01 09:46:01 +0900

    list sql err 해결
    program/movielist, program/programlist, mypage/list

--- reviewboard/src/main/java/com/example/first_project/repository/ProgramRepository.java
+++ reviewboard/src/main/java/com/example/first_project/repository/ProgramRepository.java
@@ -11,7 +11,7 @@ import com.example.first_project.model.Program;
 
 public interface ProgramRepository extends JpaRepository<Program, Long>{
 	
-	@Query(value="select p.*, avg(r.starrating) from program p RIGHT OUTER JOIN reply r ON r.bnum = p.programnum  where p.category=?1 group by r.bnum order by AVG(r.starrating)", nativeQuery = true)
+	@Query(value="select p.*, avg(r.starrating) from program p LEFT OUTER JOIN reply r ON r.bnum = p.programnum  where p.category=?1 group by r.bnum order by AVG(r.starrating)", nativeQuery = true)
 	Page<Program> findByCategory(String category, Pageable pageable);
 	
 	
