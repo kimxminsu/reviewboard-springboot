c13c7fbe53d5d410bea9ac5116f16b3281796a86
minsu <kimxminsu@gmail.com>
2021-11-01 14:37:32 +0900

    hitCount err _ complete
    조회수가 2씩 증가하는 문제 해결

--- reviewboard/src/main/java/com/example/first_project/service/ProgramService.java
+++ reviewboard/src/main/java/com/example/first_project/service/ProgramService.java
@@ -14,58 +14,57 @@ import com.example.first_project.repository.ProgramRepository;
 @Service
 public class ProgramService {
 
-	@Autowired
-	private ProgramRepository programRepository;
-	
-	
-	
-	//추가
-	public void save(Program program) {
-		programRepository.save(program);
-	}
-	public List<Program> findAll(){
-		return programRepository.findAll();
-	}
-	//변경 카테고리보기
-	public List<Program> findByCategory(String category){
-		return programRepository.findByCategory(category);
-	}
-	
-	//전체보기(페이지)
-	public Page<Program> findAll(Pageable pageable){
-		return programRepository.findAll(pageable);
-	}
-	
-	//전체보기(카테고리)
-//	public Page<Program> findByCategory(String category, Pageable pageable){
-//		return programRepository.findByCategory(category, pageable);
-//	}
+   @Autowired
+   private ProgramRepository programRepository;
+   
+   
+   
+   //추가
+   public void save(Program program) {
+      programRepository.save(program);
+   }
+   
+   //전체보기(페이지)
+   public List<Program> findAll(){
+      return programRepository.findAll();
+   }
+   
+   //전체보기(카테고리)
+   public List<Program> findByCategory(String category){
+      return programRepository.findByCategory(category);
+   }
 
-	//상세보기
-	@Transactional
-	public Program findById(Long num) {
-		Program program= programRepository.findById(num).get();
-		program.setHitcount(program.getHitcount()+1);
-		return program;
-	}
-	
-	//수정
-	@Transactional
-	public void update(Program program) {
-		Program p = programRepository.findById(program.getProgramnum()).get();
-		p.setTitle(program.getTitle());
-		p.setYear(program.getYear());
-		p.setSynopsis(program.getSynopsis());
-	}
-	
-	//삭제
-	@Transactional 
-	public void delete(Long num) {
-		programRepository.deleteById(num);
-	}
-	
-	//개수
-	public Long count() {
-		return programRepository.count();
-	}
+   //상세보기
+   @Transactional
+   public Program findById(Long num) {
+      return programRepository.findById(num).get();
+   }
+   
+   //수정
+   @Transactional
+   public void update(Program program) {
+      Program p = programRepository.findById(program.getProgramnum()).get();
+      p.setTitle(program.getTitle());
+      p.setYear(program.getYear());
+      p.setSynopsis(program.getSynopsis());
+   }
+   
+   //삭제
+   @Transactional 
+   public void delete(Long num) {
+      programRepository.deleteById(num);
+   }
+   
+   //개수
+   public Long count() {
+      return programRepository.count();
+   }
+   
+   //hitcount
+   @Transactional
+   public Program hitcount(Long num) {
+      Program program= programRepository.findById(num).get();
+      program.setHitcount(program.getHitcount()+1);
+      return program;
+   }
 }
